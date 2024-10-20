6e3d8cb6a5165a79e892f4128200ed54db39fe98
akhilomar <akhilomar555@gmail.com>
2024-10-20 19:28:22 +0530

    feature images updated

--- src/app/components/cutomActiveSlider.js
+++ src/app/components/cutomActiveSlider.js
@@ -8,40 +8,35 @@ import "swiper/css/free-mode";
 import { FreeMode, Pagination } from "swiper/modules";
 
 import { RxArrowTopRight } from "react-icons/rx";
-// import { ServiceData } from "../constants";
 
 const ServiceData = [
     {
         title : "lol",
-        backgroundImage : "/Frame31.png",
+        backgroundImage : "/algae_01.jpg",
         content: "tascvghascvas csdc sdc ds cd sc ds c"
     },
     {
         title : "lol",
-        backgroundImage : "/Frame5.png",
+        backgroundImage : "/algae_02.png",
         content: "tascvghascvas csdc sdc ds cd sc ds c"
     },
     {
         title : "lol",
-        backgroundImage : "/Frame32.png",
+        backgroundImage : "/algae_03.png",
         content: "tascvghascvas csdc sdc ds cd sc ds c"
     },
     {
         title : "lol",
-        backgroundImage : "/Frame31.png",
+        backgroundImage : "/algae_04.jpg",
         content: "tascvghascvas csdc sdc ds cd sc ds c"
     },
     {
       title : "lol",
-      backgroundImage : "/Frame5.png",
+      backgroundImage : "/algae_05.jpg",
       content: "tascvghascvas csdc sdc ds cd sc ds c"
-  },
-  {
-      title : "lol",
-      backgroundImage : "/Frame32.png",
-      content: "tascvghascvas csdc sdc ds cd sc ds c"
-  },
+  }
 ]
+
 const ActiveSlider = () => {
   return (
     <div className="flex items-center justify-center flex-col h-[300px] md:h-[567px] bg-[#1C2FDC]">
@@ -65,7 +60,26 @@ const ActiveSlider = () => {
       >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-            <img src={item.backgroundImage}/>
+            <div 
+              style={{ 
+                width: '100%', 
+                height: '100%', 
+                display: 'flex', 
+                justifyContent: 'center', 
+                alignItems: 'center' 
+              }}
+            >
+              <img 
+                src={item.backgroundImage} 
+                alt={item.title} 
+                style={{
+                  width: '100%', 
+                  height: '100%', 
+                  objectFit: 'cover', 
+                  borderRadius: '10px'
+                }}
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
